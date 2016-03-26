bcb6b274a5ad27c9ed1e7ac6d830fa59641d548f
Kephael <me@caseybaer.com>
2016-03-26 17:42:21 -0400

    -Current Services Hourly Rate now reliably displays -Modify Pricing home button fixed

--- app/view/modifyPrice.php
+++ app/view/modifyPrice.php
@@ -5,8 +5,8 @@ $result     = $mysqli->query( "SELECT * FROM Pricing WHERE id=1" ); // Must read
 $row        = mysqli_fetch_assoc( $result );
 $servicesHourlyRate = $row['servicesHourlyRate'];
 echo '<div align="right" class="btn-toolbar rightCornerButton">
-    <a ng-model="homeButton" href="../index.php#/home" class="btn btn-primary">Home</a>
-    <a ng-model="logoutButton" href="../logout.php" class="btn btn-primary">Log Out</a>
+    <a ng-model="homeButton" href="../app/index.php#/home" class="btn btn-primary">Home</a>
+        <a ng-model="logoutButton" href="../logout.php" class="btn btn-primary">Log Out</a>
 </div>
 
 <div class="container">
@@ -16,7 +16,7 @@ echo '<div align="right" class="btn-toolbar rightCornerButton">
             <form name="modifyPricingForm" method="post" action="modifyPricing.php">
                 <div class="form-group">
                     <label>CURRENT SERVICES HOURLY RATE</label>
-                    <input type="text" class="form-control" name="currentServicesHourlyRate" ng-disabled="true"  ng-model="currentServicesHourlyRate" ng-value="' . $servicesHourlyRate . '" id="currentServicesHourlyRate">
+                    <input type="text" class="form-control" name="currentServicesHourlyRate" ng-disabled="true" ng-value="' . $servicesHourlyRate . '" id="currentServicesHourlyRate">
                     <label>NEW DESIRED SERVICES HOURLY RATE</label>
                     <input type="text" class="form-control" name="newServicesHourlyRate" ng-model="newServicesHourlyRate" id="newServicesHourlyRate">
                 </div>
