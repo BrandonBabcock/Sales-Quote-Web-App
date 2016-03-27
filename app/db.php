7cc3b50769dd353056f083b21d77a560d49719ae
Kephael <me@caseybaer.com>
2016-03-27 19:15:36 -0400

    -Made a single, dynamic landing page for login

--- app/db.php
+++ app/db.php
@@ -5,5 +5,5 @@ $username = "admin_salesform"; // Mysql username
 $password = "mqJoPHm9LEz"; // Mysql password
 $db_name  = "admin_salesform"; // Database name
 $mysqli = new mysqli( $host, $username, $password, $db_name );
-
+$dbh = new PDO('mysql:host=' . $host . ';' . 'dbname=' . $db_name, $username, $password);
 ?>
--- app/view/userResults.php
+++ app/view/userResults.php
@@ -6,46 +6,84 @@ echo
     <a ng-model="homeButton" href="../app/index.php#/home" class="btn btn-primary">Home</a>
     <a ng-model="logoutButton" href="../app/logout.php" class="btn btn-primary">Log Out</a>
 </div>';
-$rec_limit = 10;
-
-/* Get total number of records */
-$sql    = "SELECT count(username) FROM users ";
-$retval = $mysqli->query( $sql );
-
-$row       = mysqli_fetch_row( $retval );
-$rec_count = $row[0];
-
-if ( isset( $_GET{'page'} ) ) {
-	$page   = $_GET{'page'} + 1;
-	$offset = $rec_limit * $page;
-} else {
-	$page   = 0;
-	$offset = 0;
-}
-
-$left_rec = $rec_count - ( $page * $rec_limit );
 $searchValue = $_SESSION['lastsearch'];
-$sql      = "SELECT username, admin " .
-            "FROM users " .
-            "WHERE username LIKE '%$searchValue%'" .
-            "LIMIT $offset, $rec_limit";
 
-$retval = $mysqli->query( $sql );
+try {
 
-while ( $row = mysqli_fetch_assoc( $retval ) ) {
-	echo "Username :{$row['username']}  <br> " .
-	     "Administrator : {$row['admin']} <br> " .
-	     "--------------------------------<br>";
+	// Find out how many items are in the table
+	$total = $dbh->query("
+        SELECT count(username) FROM users WHERE username LIKE '%$searchValue%'
+    ")->fetchColumn();
+
+	// How many items to list per page
+	$limit = 10;
+
+	// How many pages will there be
+	$pages = ceil($total / $limit);
+
+	// What page are we currently on?
+	$page = min($pages, filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT, array(
+		'options' => array(
+			'default'   => 1,
+			'min_range' => 1,
+		),
+	)));
+
+	// Calculate the offset for the query
+	$offset = ($page - 1)  * $limit;
+
+	// Some information to display to the user
+	$start = $offset + 1;
+	$end = min(($offset + $limit), $total);
+
+	// The "back" link
+	$prevlink = ($page > 1) ? '<a href="?page=1#/userResults" title="First page">&laquo;</a> <a href="?page=' . ($page - 1) . '#/userResults' . '" title="Previous page">&lsaquo;</a>' : '<span class="disabled">&laquo;</span> <span class="disabled">&lsaquo;</span>';
+
+	// The "forward" link
+	$nextlink = ($page < $pages) ? '<a href="?page=' . ($page + 1) . '#/userResults' . '" title="Next page">&rsaquo;</a> <a href="?page=' . $pages . '#/userResults' . '" title="Last page">&raquo;</a>' : '<span class="disabled">&rsaquo;</span> <span class="disabled">&raquo;</span>';
+
+	// Display the paging information
+	echo '<div id="paging"><p>', $prevlink, ' Page ', $page, ' of ', $pages, ' pages, displaying ', $start, '-', $end, ' of ', $total, ' results ', $nextlink, ' </p></div>';
+
+	// Prepare the paged query
+	$stmt = $dbh->prepare("
+        SELECT 
+            username, admin 
+         FROM 
+            users 
+        WHERE 
+            username LIKE '%$searchValue%'
+        ORDER BY
+            username
+        LIMIT
+            :limit
+        OFFSET
+            :offset
+    ");
+
+	// Bind the query params
+	$stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+	$stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+	$stmt->execute();
+
+	// Do we have any results?
+	if ($stmt->rowCount() > 0) {
+		// Define how we want to fetch the results
+		$stmt->setFetchMode(PDO::FETCH_ASSOC);
+		$iterator = new IteratorIterator($stmt);
+
+		// Display the results
+		foreach ($iterator as $row) {
+			echo '<p>Username: ', $row['username'], '</p>';
+			echo '<p>Administrator: ', $row['admin'], '</p>';
+		}
+
+	} else {
+		echo '<p>No results could be displayed.</p>';
+	}
+
+} catch (Exception $e) {
+	echo '<p>', $e->getMessage(), '</p>';
 }
-$PHP_SELF = &$_SERVER['PHP_SELF'];
-if ( $page > 0 ) {
-	$last = $page - 2;
-	echo "<a href = \"index.php#/userResults?page = $last\">Last 10 Records</a> |";
-	echo "<a href = \"index.php#/userResults?page = $page\">Next 10 Records</a>";
-} else if ( $page == 0 ) {
-	echo "<a href = \"index.php#/userResults?page = $page\">Next 10 Records</a>";
-} else if ( $left_rec < $rec_limit ) {
-	$last = $page - 2;
-	echo "<a href = \"index.php#/userResults?page = $last\">Last 10 Records</a>";
-}
+
 ?>
