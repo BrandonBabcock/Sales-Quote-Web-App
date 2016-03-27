e025c18b3e97ed5cc69287529aac5d882a7355b9
Kephael <me@caseybaer.com>
2016-03-27 17:28:54 -0400

    -Added paginated user search system

---
+++ app/usersearch.php
@@ -0,0 +1,13 @@
+<?php
+require('db.php');
+session_start();
+if ($_SESSION['admin'] != 'true') {
+	echo '<H1>Access Denied</H1>';
+	exit(2);
+}
+$data = array();
+parse_str( file_get_contents( 'php://input' ), $data );
+$_POST = array_merge( $data, $_POST ); // merge parsed login data with _POST session values
+var_dump($GLOBALS);
+$_SESSION['lastsearch'] = mysqli_real_escape_string($mysqli, $_POST['searchUsers']);
+header("location:index.php#/userResults");
--- app/view/userResults.php
+++ app/view/userResults.php
@@ -1,7 +1,51 @@
 <?php
+require( '../db.php' );
+session_start();
 echo
 '<div align="right" class="btn-toolbar rightCornerButton">
     <a ng-model="homeButton" href="../app/index.php#/home" class="btn btn-primary">Home</a>
     <a ng-model="logoutButton" href="../app/logout.php" class="btn btn-primary">Log Out</a>
 </div>';
+$rec_limit = 10;
+
+/* Get total number of records */
+$sql    = "SELECT count(username) FROM users ";
+$retval = $mysqli->query( $sql );
+
+$row       = mysqli_fetch_row( $retval );
+$rec_count = $row[0];
+
+if ( isset( $_GET{'page'} ) ) {
+	$page   = $_GET{'page'} + 1;
+	$offset = $rec_limit * $page;
+} else {
+	$page   = 0;
+	$offset = 0;
+}
+
+$left_rec = $rec_count - ( $page * $rec_limit );
+$searchValue = $_SESSION['lastsearch'];
+$sql      = "SELECT username, admin " .
+            "FROM users " .
+            "WHERE username LIKE '%$searchValue%'" .
+            "LIMIT $offset, $rec_limit";
+
+$retval = $mysqli->query( $sql );
+
+while ( $row = mysqli_fetch_assoc( $retval ) ) {
+	echo "Username :{$row['username']}  <br> " .
+	     "Administrator : {$row['admin']} <br> " .
+	     "--------------------------------<br>";
+}
+$PHP_SELF = &$_SERVER['PHP_SELF'];
+if ( $page > 0 ) {
+	$last = $page - 2;
+	echo "<a href = \"index.php#/userResults?page = $last\">Last 10 Records</a> |";
+	echo "<a href = \"index.php#/userResults?page = $page\">Next 10 Records</a>";
+} else if ( $page == 0 ) {
+	echo "<a href = \"index.php#/userResults?page = $page\">Next 10 Records</a>";
+} else if ( $left_rec < $rec_limit ) {
+	$last = $page - 2;
+	echo "<a href = \"index.php#/userResults?page = $last\">Last 10 Records</a>";
+}
 ?>
