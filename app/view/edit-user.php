c10c1466aeec0b4d544ba2f2624b2ef5e5cef6ff
me <me@caseybaer.com>
2016-04-15 14:51:46 -0400

    -Added check to only edit valid user account

--- app/view/edit-user.php
+++ app/view/edit-user.php
@@ -17,11 +17,50 @@ if ($_SESSION['admin'] != 'true') { // non-administrator tried to access page
 $user = filter_input(INPUT_GET, 'user', FILTER_SANITIZE_STRING);
 $user = mysqli_real_escape_string($mysqli, $user);
 $_SESSION['userToModify'] = $user;
+$sql = $dbh->prepare("
+        SELECT
+              admin, enabled
+        FROM
+              users
+        WHERE
+              username = :username
+        ");
+$sql->bindParam(":username", $user, PDO::PARAM_STR);
+$valid = $sql->execute();
+if ($sql->rowCount() < 1) { // user doesn't exist
+    echo '<script type="text/javascript">
+        alert ("user does not exist!");
+        window.location = "index.php#/users";
+</script>';
+}
+$results = $sql->fetch(PDO::FETCH_ASSOC);
+$accountEnabled;
+$accountDisabled;
+if ($results['enabled'] == 'true') {
+    $accountEnabled = 'true';
+    $accountDisabled = 'false';
+} else {
+    $accountEnabled = 'false';
+    $accountDisabled = 'true';
+}
+$adminEnabled;
+$adminDisabled;
+if ($results['admin'] == 'true') {
+    $adminEnabled = 'true';
+    $adminDisabled = 'false';
+} else {
+    $adminEnabled = 'false';
+    $adminDisabled = 'true';
+}
 echo '
 <!-- Link stylesheets -->
 <!-- Documentation for second style sheet: https://bootswatch.com/darkly/ -->
 <link rel="stylesheet" href="../../assets/css/style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/darkly/bootstrap.css">
+<!-- Load in Global Dependencies -->
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.18/angular-ui-router.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular-animate.min.js"></script>
 
 <div align="right" class="btn-toolbar rightCornerButton">
     <a ng-model="homeButton" href="../index.php#/home" class="btn btn-primary">Home</a>
@@ -38,9 +77,9 @@ echo '
                 <fieldset>
                     <div class="form-group-single">
                         <label>ENABLED STATUS</label>
-                        <select name="enabled" class="select-box" title="true grants the user access to the application" ng-value="">
-                            <option>true</option>
-                            <option>false</option>
+                        <select name="enabled" class="select-box" title="true grants the user access to the application">
+                            <option ng-selected="' . $accountEnabled . '">true</option>
+                            <option ng-selected="' . $accountDisabled . '">false</option>
                         </select>
                     </div>
 
@@ -48,9 +87,9 @@ echo '
 
                     <div class="form-group-single">
                         <label>ADMIN STATUS</label>
-                        <select name="adminStatus" class="select-box" title="true grants the user admin privileges" ng-value="">
-                            <option>true</option>
-                            <option>false</option>
+                        <select name="adminStatus" class="select-box" title="true grants the user admin privileges">
+                            <option ng-selected="' .  $adminEnabled . '">true</option>
+                            <option ng-selected="' .  $adminDisabled . '">false</option>
                         </select>
                     </div>
 
